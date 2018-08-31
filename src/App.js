38d392f277beb4575914a5a41b5186824d484589
Alex Gjorvad <alexgjorvad@gmail.com>
2018-08-30 19:05:48 -0500

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GallerySelect from './components/GallerySelect/GallerySelect';
+import myTour from './components/myTour/myTour';
+import Exhibits from './components/Exhibits/Exhibits';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-      <GallerySelect/>
-
+      <Router>
+      <Route
+        path="/"
+        component={myTour} />
+        <Route
+        path="/exhibits"
+        component={Exhibits} />
+        <Route
+        path="/gallery"
+        component={GallerySelect} />
+</Router>
       </div>
     );
   }
